c45950399168d54d4b37c3b08cecf02a9cb55aa9
shuknuk <kg467@outlook.com>
2024-07-05 18:58:16 -0400

    description update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="relative z-10 p-10 text-center text-white">
         <h1 className="text-4xl font-bold mb-12">Operation Elementary</h1>
         <p className="mb-8">
-          Our city will grow stronger when every New Orleanian can contribute to our wellbeing and innovation through the tools of math and science.
+        Enhancing the childhood experience
         </p>
         <button className="bg-orange-500 text-white px-4 py-2 rounded">Learn More</button>
       </div>
