bfa9b70468ac6ea64e34f4b154c1d77413ada77c
shuknuk <kg467@outlook.com>
2024-07-05 18:44:00 -0400

    cleanup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className="bg-blue-900 text-white p-4 flex justify-between items-center">
       <div className="text-2xl font-bold">SciHigh</div>
       <nav className="space-x-4">
-        <Link href="#"><span className="cursor-pointer">Community</span></Link>
-        <Link href="#"><span className="cursor-pointer">Athletics</span></Link>
-        <Link href="#"><span className="cursor-pointer">Calendar</span></Link>
-        <Link href="#"><span className="cursor-pointer">Employment</span></Link>
-        <Link href="#"><span className="cursor-pointer">Translate</span></Link>
-        <Link href="#"><span className="cursor-pointer">Donate</span></Link>
+        <a href="#" className="cursor-pointer">Community</a>
+        <a href="#" className="cursor-pointer">Athletics</a>
+        <a href="#" className="cursor-pointer">Calendar</a>
+        <a href="#" className="cursor-pointer">Employment</a>
+        <a href="#" className="cursor-pointer">Translate</a>
+        <a href="#" className="cursor-pointer">Donate</a>
       </nav>
     </header>
   );
